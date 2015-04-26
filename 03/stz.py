e1a195ffab7fd4fd14b29af2aae246c3c80b9317
stzstzstz11 <stzstzstz@yeah.net>
2015-04-26 15:19:29 +0800

    03

---
+++ 03/stz.py
@@ -0,0 +1,20 @@
+def BinSearch(list,value):
+    begin = 0
+    if list[0] == value:
+        return 0
+
+    end = len(list)-1
+    while(begin < end):
+        mid = begin + (end - begin)//2
+        if list[mid]>value:
+            end = mid - 1
+        elif list[mid] <value:
+            begin = mid + 1
+        else:
+            return mid
+
+    if(begin == end):
+        if(value == list[begin]):
+            return begin
+        else:
+            return -1
